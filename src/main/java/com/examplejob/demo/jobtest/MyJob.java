dc6b07162ee524302265fbd7f2a05fffe5dcb4b0
徐礼华 <xulihua@52mamahome.com>
2018-12-21 17:39:47 +0800

    Initial commit

---
+++ src/main/java/com/examplejob/demo/jobtest/MyJob.java
@@ -0,0 +1,16 @@
+package com.examplejob.demo.jobtest;
+
+import com.dangdang.ddframe.job.api.ShardingContext;
+import com.dangdang.ddframe.job.api.simple.SimpleJob;
+import org.springframework.stereotype.Component;
+
+@ElasticSimpleJob()
+@Component
+public class MyJob implements SimpleJob {
+
+    @Override
+    public void execute(ShardingContext shardingContext) {
+
+    }
+
+}
